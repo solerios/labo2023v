c89dcfc9f0055096ecaa9277f075e75a345d0692
VeronicaGarciaRio <veronicagarciario@gmail.com>
2023-05-24 18:48:30 -0300

    MODELO1

--- Modelos/z741_TS_training_strategy_MODELO1.r
+++ Modelos/z741_TS_training_strategy_MODELO1.r
@@ -18,7 +18,7 @@ require("yaml")
 PARAM  <- list()
 PARAM$experimento <- "TS7410"
 
-PARAM$exp_input  <- "FE7310"
+PARAM$exp_input  <- "FE73100"
 
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
