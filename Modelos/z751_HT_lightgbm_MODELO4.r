3761d851dbe72d2ffd83e788f5b8e5a647369641
VeronicaGarciaRio <veronicagarciario@gmail.com>
2023-05-25 08:37:41 -0300

    MODELO4

--- Modelos/z751_HT_lightgbm_MODELO4.r
+++ Modelos/z751_HT_lightgbm_MODELO4.r
@@ -79,7 +79,7 @@ PARAM$bo_lgb <- makeParamSet(
          makeNumericParam("learning_rate",    lower=    0.02, upper=     0.3),
          makeNumericParam("feature_fraction", lower=    0.01, upper=     1.0),
          makeIntegerParam("num_leaves",       lower=    8L,   upper=  1024L),
-         makeIntegerParam("min_data_in_leaf", lower=  100L,   upper= 50000L)
+         makeIntegerParam("min_data_in_leaf", lower=  100L,   upper= 50000L),
          makeNumericParam("lambda_l1",        lower=    0.0,    upper=   1000.0),
          makeNumericParam("lambda_l2",        lower=    0.0,    upper=   1000.0),
          makeNumericParam("min_gain_to_split",lower=    0.0,    upper=     20.0))
