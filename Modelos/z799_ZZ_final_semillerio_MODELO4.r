e78d88e27b4e4a40b24aca3ba7de940c09f84e92
VeronicaGarciaRio <veronicagarciario@gmail.com>
2023-05-25 09:24:09 -0300

    MODELO4

--- Modelos/z799_ZZ_final_semillerio_MODELO4.r
+++ Modelos/z799_ZZ_final_semillerio_MODELO4.r
@@ -25,7 +25,7 @@ PARAM$exp_input  <- "HT7510_4 "
 
 #Atencion, que cada modelos se procesa con 5 semillas, ajuste a SUS necesidades.
 PARAM$modelos_rank  <- c( 1 )  #Que modelos quiero, segun su posicion en el ranking e la Bayesian Optimizacion, ordenado por ganancia descendente
-PARAM$semillerio  <- 50   # cantidad de semillas a utilizar en el semillerio, podrian ser 50 o 100 para mayor estabilidad
+PARAM$semillerio  <- 20   # cantidad de semillas a utilizar en el semillerio, podrian ser 50 o 100 para mayor estabilidad
 PARAM$semilla_primos  <- 100169  # se utiliza para generar el vector de  PARAM$semillerio  semillas
 
 PARAM$kaggle$envios_desde  <-  8000L
