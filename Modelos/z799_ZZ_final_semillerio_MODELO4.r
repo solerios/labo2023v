6efcbc13259c88f01a08a458a6e7acd0dfefa2dd
VeronicaGarciaRio <veronicagarciario@gmail.com>
2023-05-26 02:33:38 -0300

    MODELO4

--- Modelos/z799_ZZ_final_semillerio_MODELO4.r
+++ Modelos/z799_ZZ_final_semillerio_MODELO4.r
@@ -21,9 +21,9 @@ require("lightgbm")
 #Parametros del script
 PARAM  <- list()
 PARAM$experimento  <- "ZZ7990_4"
-PARAM$exp_input  <- "HT7510_4 "
+PARAM$exp_input  <- "HT7510_4"
 
-#Atencion, que cada modelos se procesa con 5 semillas, ajuste a SUS necesidades.
+#Atencion, que cada modelos se procesa con 5 semillas, ajuste a SUS necesidades
 PARAM$modelos_rank  <- c( 1 )  #Que modelos quiero, segun su posicion en el ranking e la Bayesian Optimizacion, ordenado por ganancia descendente
 PARAM$semillerio  <- 20   # cantidad de semillas a utilizar en el semillerio, podrian ser 50 o 100 para mayor estabilidad
 PARAM$semilla_primos  <- 100169  # se utiliza para generar el vector de  PARAM$semillerio  semillas
