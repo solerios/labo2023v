65ec20ea7619fb3abe06e213df23d4d520e49eed
VeronicaGarciaRio <veronicagarciario@gmail.com>
2023-05-27 12:51:42 -0300

    baseline sep

--- Modelos/z741_TS_training_strategy_baselinek.r
+++ Modelos/z741_TS_training_strategy_baselinek.r
@@ -16,9 +16,9 @@ require("yaml")
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento <- "TS7410_baseline"
+PARAM$experimento <- "TS7410_baselinee"
 
-PARAM$exp_input  <- "FE7310"
+PARAM$exp_input  <- "FE73100"
 
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
