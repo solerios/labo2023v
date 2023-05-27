b2a70ac84fb45bfdb2fa504c85b7548ccb29b3f6
VeronicaGarciaRio <veronicagarciario@gmail.com>
2023-05-27 16:33:42 -0300

    baselinek

--- Modelos/z741_TS_training_strategy_baselinek.r
+++ Modelos/z741_TS_training_strategy_baselinek.r
@@ -31,7 +31,7 @@ PARAM$train$validation   <- c( 202106 )
 PARAM$train$testing      <- c( 202107 )
 
 # Atencion  0.4  de  undersampling de la clase mayoritaria,  los CONTINUA
-PARAM$train$undersampling  <- 0.4   # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
+PARAM$train$undersampling  <- 0.1   # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
 
 PARAM$train$semilla  <- 100169  # cambiar por su propia semilla  !!!
 
